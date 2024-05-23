ad4a63927257f197b20e88a055893b298cbe5218
GrantHen <grantscotthenderson@gmail.com>
2024-05-23 01:45:11 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,35 @@
+using System;
+
+public class Program {
+  public static void Main (string[] args) 
+  {
+    Console.WriteLine("Oh, well imagine /");
+    Console.WriteLine("As I'm pacing the pews in a church corridor /");
+    Console.WriteLine("And I can't help but to hear /");
+    Console.WriteLine("No, I can't help but to hear an exchanging of words /");
+
+    Console.WriteLine();
+
+    Console.WriteLine("Oh-oh-oh-oh-oh,");
+    Console.WriteLine("oh-oh-oh-oh,");
+    Console.WriteLine("oh-oh-oh /");
+    Console.WriteLine("Caught in a bad romance...");
+
+    Console.WriteLine();
+
+    Console.WriteLine("Coming out of my cage");
+    Console.WriteLine("And I've been doing just fine");
+    Console.WriteLine("Gotta gtta be down");
+    Console.WriteLine("Because I want it all");
+
+    Console.WriteLine();
+
+    Console.WriteLine("Now, UnauthorizedAccessException is a store all about how");
+    Console.WriteLine("My life got flipped-turned upside down");
+    Console.WriteLine("And I'd like to take a minute");
+    Console.WriteLine("Just sit right there");
+    Console.WriteLine("I'll tell you how I became the prince of a town called Bel-Air");
+
+
+  }
+}
